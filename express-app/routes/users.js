ecadc4ff65cdc1b411413042ecc3006fcf48f905
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2026-03-24 17:02:08 +0500

    task 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({
+    items: [
+      {
+        "id" : 1,
+        "name" : "Степан Григорьев",
+      },
+      {
+        "id" : 2,
+        "name" : "Александр Левенских"
+      }
+    ]
+  });
 });
 
 module.exports = router;
